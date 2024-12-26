1f274b49bf3742f66238a7c6d080089a2c0ffc81
Rishabh Kumar <119288381+AlieenBotrishabh@users.noreply.github.com>
2024-12-26 12:21:38 +0530

    register

--- form.js
+++ form.js
@@ -49,6 +49,14 @@ app.put('/api/people/:id', (req, res) => {
     res.status(200).json({ success : true, data : newPeople})
 });
 
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id));
+    if(!person)
+    {
+        return res.status(404).json({ success : false, msg : `person with id ${req.params.id}`});
+    }
+})
+
 app.listen(5000, () => {
     console.log('Server is listening on the port 5000');
 });
